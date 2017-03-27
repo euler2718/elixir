ed5eb52951a500ccc6cf110d7397bb2f8397fd88
John Corley <johndcorley13@gmail.com>
2017-03-27 13:56:50 -0400

    Chapter 7 + 8 (not finished)

--- VMTranslator.exs
+++ VMTranslator.exs
@@ -40,13 +40,13 @@ defmodule Vmtranslator do
 		args |> parse_args |> parsing
 	end
 
-  defp parse_args(args) do
-    # Covering only expected arguments. We can add code to handle cases when unexpected arguments are passed or help text etc.
-    # Checkout OptionParser lib for details. 
-    {_, b, _} = OptionParser.parse(args)
-    c = Path.absname(b, System.cwd!)
-    ~s/#{c}/
-  end
+  	defp parse_args(args) do
+    	# Covering only expected arguments. We can add code to handle cases when unexpected arguments are passed or help text etc.
+    	# Checkout OptionParser lib for details. 
+    	{_, b, _} = OptionParser.parse(args)
+    	c = Path.absname(b, System.cwd!)
+    	~s/#{c}/
+  	end
 
 	def parsing(file) do
 		#worded = ~s/#{file}/
@@ -63,6 +63,20 @@ defmodule Vmtranslator do
 			|> writeFile(path)
 	end
 
+	def find_func( _path, function) do
+		#this is where I need to find a specific function within a "Class.vm" file
+		#this function is definitely NOT finished.  How to know when to stop wth func in class file? -> @return line
+		#base_path = Path.absname(path)
+		file = ( ( String.split(function, ".") |> hd ) <> ".vm" )
+		[_, str] = Regex.run( ~r/function #{function} (.*)return/s, File.read!(file) )
+		str
+			|> String.split("\r\n")
+			|> Stream.filter( &( String.match?( &1, ~r{^\w.+$} ) ))
+			|> Stream.with_index
+			|> Stream.map( &(clean(&1, function) ) )
+			|> Enum.reduce( &( &2 <> &1 ) )
+	end
+
 	@doc """
 	tuple test
 	
@@ -74,6 +88,8 @@ defmodule Vmtranslator do
 
 	def clean({thez, ind}, path) do
 		thez
+			|> String.split("//", trim: true)
+			|> hd
 			|> String.trim_trailing
 			|> String.split(" ")
 			|> tuple
@@ -105,13 +121,16 @@ defmodule Vmtranslator do
 			@0
 			M=M+1
 			"""
+			{ "function", fn_name, num_locals } -> _function({ "function", fn_name, num_locals }, ind, path )
+			{ "call", fn_name, num_args } -> _function({ "call", fn_name, num_args }, ind, path ) #need to locate the vm file
+			{ "return" } -> _function({"return"}, path )
 			{ flow, string } -> cmdParse( { flow, string }, path)
-			{ cmd, loc, qty } -> cmdParse( { cmd, loc, qty }, path )
+			{ cmd, loc, qty } -> cmdParse( { cmd, loc, qty }, path, ind )
 			{ g } -> arithmetic( g, ind )
 		end
 	end
 
-	def cmdParse({a,b,c}, path) do
+	def cmdParse({a,b,c}, path, ind) do
 		case b do
 			"local" -> _cmdParse( {a, "@LCL\nA=M", c } )	
 			"argument" -> _cmdParse( { a, "@ARG\nA=M", c } )
@@ -119,16 +138,15 @@ defmodule Vmtranslator do
 			"that" -> _cmdParse( { a, "@THAT\nA=M", c } )
 	 		"temp" -> _cmdParse( { a, "@R5", c } )
 	 		"pointer" -> _cmdParse( { a, "@R3", c } )
-			"static" -> _cmdParse( { a, "@#{Path.basename(path) |> Path.rootname}.#{c}", 0 } )
+			"static" -> _cmdParse( { a, "@#{Path.basename(path) |> Path.rootname}#{ind}.#{c}", 0 } ) #Path.basename(path) |> Path.rootname
 		end
 	end
 
-	def cmdParse( {flow, string }, path ) do
+	def cmdParse( {flow, string }, _path ) do
 		case flow do
 			"label" -> "(#{string})\n"
 			"goto" -> "@" <> string <> "\n0;JMP\n"
-			#assuming the prior conditional left a boolean
-			"if-goto" -> "@SP\nA=M-1\nD=M\n@1\nD=D+A\n@#{string}\nD;JEQ\n"
+			"if-goto" -> "@SP\nM=M-1\nA=M\nD=M\n@#{string}\nD;JNE\n"
 		end
 	end
 
@@ -156,6 +174,87 @@ defmodule Vmtranslator do
 			"neg" -> "@SP\nA=M-1\nM=-M\n"
 			"not" -> "@SP\nA=M-1\nM=!M\n"
 		end
+	end
+
+	defp _function( { a, b, n_args }, ind, _path ) do
+		#base_path = Path.basename(path)
+		duple = """
+		D=M
+		@SP
+		A=M
+		M=D
+		@SP
+		M=M+1
+		""" 
+			#|> String.trim_trailing
+		case a do
+			"function" -> "@261\nD=A\n@SP\nM=D\n" <> String.duplicate("A=M\nM=0\n@SP\nM=M+1\n", n_args) # function commands, then return
+			"call" -> """
+			@#{b}$#{ind}
+			D=A
+			@SP
+			A=M
+			M=D
+			@SP
+			M=M+1
+			(#{b}$#{ind})
+			#{Enum.join(["@LCL", "@ARG", "@THIS", "@THAT"], "\n" <> duple)}
+			"""
+				<>"@SP\nD=M-1\n"
+				<> String.duplicate("D=D-1\n", 5+n_args-1) <> 
+			"""
+			@ARG
+			M=D
+			@SP
+			D=M
+			@LCL
+			M=D
+			@SP
+			""" 
+				<> String.duplicate("A=M\nM=0\n@SP\nM=M+1\n", n_args)
+				<> ( parse_args( [( String.split(b, ".") |> hd ) <> ".vm"] ) |> find_func(b) ) <> 
+			"""
+			@LCL
+			D=M
+			@R5
+			M=D
+			@R6
+			M=D-1
+			@4
+			D=A
+			@R6
+			M=M-D
+			"""
+				<> cmdParse({"pop","argument",0}, "", "FUNC") <>
+			"""
+			@ARG
+			D=A+1
+			@SP
+			A=M
+			M=D
+			@R5
+			D=M
+			D=D-1
+			@THAT
+			M=D
+			D=D-1
+			@THIS
+			M=D
+			D=D-1
+			@ARG
+			M=D
+			D=D-1
+			@LCL
+			M=D
+			@R5
+			A=M
+			0;JMP
+			"""
+		end	
+	end
+
+	defp _function( { _ }, _path) do
+		""
 	end
 
 	def writeFile( string, path ) do
